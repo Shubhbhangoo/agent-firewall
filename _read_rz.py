e82c599ec76a5bf032c05bafbc07664340c3ed43
shubhbhangoo <shubhbhangoo@gmail.com>
2026-08-28 22:51:40 +0530

    chore: remove scratch file

--- _read_rz.py
+++
@@ -1,5 +0,0 @@
-import io, sys
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
-s = io.open("firewall/authorization.py", encoding="utf-8").read().splitlines()
-for i in range(145, 205):
-    print(i + 1, s[i])
