f5c3166f10dddcf1dccf6d2c1d62fcf0331b5cf3
shubhbhangoo <shubhbhangoo@gmail.com>
2026-08-28 23:27:32 +0530

    chore: remove scratch files that slipped into the docs commit

--- _peek.py
+++
@@ -1,5 +0,0 @@
-import io, sys
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
-s = io.open("firewall/ui/static/control.js", encoding="utf-8").read()
-print("lines:", len(s.splitlines()))
-print(s[:900])
